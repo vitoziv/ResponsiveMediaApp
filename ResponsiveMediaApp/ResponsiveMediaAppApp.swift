6ddb7a4fbb0bc63f57a3cd037fd04f00c469e0ee
Vito <vvitozhang@gmail.com>
2022-06-19 19:41:00 +0800

    Initial Commit

---
+++ ResponsiveMediaApp/ResponsiveMediaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResponsiveMediaAppApp.swift
+//  ResponsiveMediaApp
+//
+//  Created by Vito on 2022/6/19.
+//
+
+import SwiftUI
+
+@main
+struct ResponsiveMediaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
